ada9cbcd737f0681104bc3e5087c7d081c72d153
YuliiaBabich <aim2002@ukr.net>
2024-02-25 23:47:02 +0200

    1

--- script.js
+++ script.js
@@ -1,87 +1,28 @@
-:root {
-  --light-grey: #f5f6f7;
-  --dark-blue: #0a0a23;
-  --fcc-blue: #1b1b32;
-  --light-yellow: #fecc4c;
-  --dark-yellow: #feac32;
-  --light-pink: #ffadad;
-  --dark-red: #850000;
-  --light-green: #acd157;
-}
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false; // is и has  мы используем для boolean
+/*const cleanInputString = function (str) { //imput элементы все равно считывают как строку, даже если и задали namber нам нужна функция , чтобы преобразовывать рядок в число
+  const strArray = str.split(``);
+  const cleanStrArray = [];
+  for (let i = 0; i < strArray.length; i++) {
+    if (!['+', '-', ' '].includes(strArray[i])) {
+      cleanStrArray.push(strArray[i])
+    }
+  };
+}*/
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+ return str.replace (regex,"");
 
-body {
-  font-family: "Lato", Helvetica, Arial, sans-serif;
-  font-size: 18px;
-  background-color: var(--fcc-blue);
-  color: var(--light-grey);
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+   return str.match(regex);
 }
-
-h1 {
-  text-align: center;
 }
-
-.container {
-  width: 90%;
-  max-width: 680px;
-}
-
-h1,
-.container,
-.output {
-  margin: 20px auto;
-}
-
-label,
-legend {
-  font-weight: bold;
-}
-
-.input-container {
-  display: flex;
-  flex-direction: column;
-}
-
-button {
-  cursor: pointer;
-  text-decoration: none;
-  background-color: var(--light-yellow);
-  border: 2px solid var(--dark-yellow);
-}
-
-button,
-input,
-select {
-  min-height: 24px;
-  color: var(--dark-blue);
-}
-
-fieldset,
-label,
-button,
-input,
-select {
-  margin-bottom: 10px;
-}
-
-.output {
-  border: 2px solid var(--light-grey);
-  padding: 10px;
-  text-align: center;
-}
-
-.hide {
-  display: none;
-}
-
-.output span {
-  font-weight: bold;
-  font-size: 1.2em;
-}
-
-.surplus {
-  color: var(--light-pink);
-}
-
-.deficit {
-  color: var(--light-green);
+function addEntry() {
+   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
 }
